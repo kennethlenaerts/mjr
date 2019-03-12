4ca1794fca2b8dfbcf4dccdbeabaf803bfdaa2d4
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2019-03-12 22:26:26 +0100

    Add user stats component

--- src/app/components/user-stats/user-stats.component.ts
+++ src/app/components/user-stats/user-stats.component.ts
@@ -6,17 +6,32 @@ import { Component, OnInit, Input } from '@angular/core';
     <div class="info-frame">
       <div class="content">
         <div class="user-info">
-          <h1>{{ userName }}</h1>
-          <h1>Level {{ level }}</h1>
+          <h2>{{ userName }}</h2>
+          <h2>Level {{ level }}</h2>
         </div>
-        <!-- use rare candy? -->
 
         <div class="value" *ngFor="let item of (userStats | keyvalue)">
-          <h2>{{ item.key }}</h2>
-          <h2>{{ item.value }}</h2>
+          <img
+            [src]="'assets/stat_info_icons/stat_info_ic_' + item.key + '.png'"
+          />
+          <h3>{{ item.key }}</h3>
+          <h3>{{ item.value }}</h3>
         </div>
       </div>
     </div>
+    <!--
+
+    <div class="user-info">
+      <h1>{{ userName }}</h1>
+      <h1>Level {{ level }}</h1>
+    </div>
+
+    <div class="value" *ngFor="let item of (userStats | keyvalue)">
+      <img [src]="'assets/stat_info_icons/stat_info_ic_' + item.key + '.png'" />
+      <h2>{{ item.key }}</h2>
+      <h2>{{ item.value }}</h2>
+    </div>
+    -->
   `,
   styleUrls: ['user-stats.component.scss']
 })
@@ -28,7 +43,7 @@ export class UserStatsComponent implements OnInit {
     defense: number;
     health: number;
     gold: number;
-    gems: number;
+    gem: number;
   };
 
   ngOnInit() {
@@ -39,7 +54,7 @@ export class UserStatsComponent implements OnInit {
       defense: 1234,
       health: 1234,
       gold: 1234,
-      gems: 1234
+      gem: 1234
     };
   }
 }
