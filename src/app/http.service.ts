9f6eba0b5236c306eb62e43e350b01bec7626b7b
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2019-05-17 17:53:18 +0200

    Create http service for itemms

---
+++ src/app/http.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Item } from "./models";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class HttpService {
+  private url: string = "localhost:3000";
+  constructor(private http: HttpClient) {}
+
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(`${this.url}/items`);
+  }
+}
