8765d9c68cbe1f61c892820baef177cc9a6c6f5a
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2019-05-17 17:36:41 +0200

    Add mock server

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,10 +1,11 @@
 export enum ItemType {
-  health,
-  weapon,
-  misc,
+  health = 1,
+  weapon = 2,
+  misc = 3,
 }
 
 export interface Item {
+  id: number;
   name: string;
   src: string;
   value: number;
