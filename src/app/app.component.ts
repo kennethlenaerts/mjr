bf2cefaa2ed90bb0a52ca7422128a8488882d873
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2019-05-31 23:45:07 +0200

    Fix state in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,26 @@
 import { HttpService } from './http.service';
+import { State } from './state';
+import { selectPlayerStats } from './state/player';
 import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Player } from '@app/models';
-import { PlayerState } from '@app/player.state';
 import { slider } from '@app/route-animations';
-import { Select } from '@ngxs/store';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 @Component({
   selector: "app-root",
   template: `
-    <!--
     <app-header [playerStats]="playerStats$ | async"></app-header>
-    -->
 
     <main [@routeAnimations]="prepareRoute(outlet)">
       <router-outlet #outlet="outlet"></router-outlet>
     </main>
 
-    <!--
     <app-footer></app-footer>
 
     <button *ngIf="devTools" (click)="resetDb()" class="reset">Reset DB</button>
-    -->
   `,
   styleUrls: ["app.component.scss"],
   animations: [slider],
@@ -31,13 +28,15 @@ import { take } from 'rxjs/operators';
 export class AppComponent {
   devTools: boolean = false;
 
-  @Select(PlayerState.stats) playerStats$: Observable<Partial<Player>>;
+  playerStats$: Observable<Partial<Player>>;
 
   @HostListener("document:keydown.w") showDevTools() {
     this.devTools = !this.devTools;
   }
 
-  constructor(private _httpService: HttpService) {}
+  constructor(private _httpService: HttpService, private store: Store<State>) {
+    this.playerStats$ = store.pipe(select(selectPlayerStats));
+  }
 
   prepareRoute(outlet: RouterOutlet) {
     return (
